2de704b29b42320c55210515ff38f1422e6d809a
zoya tretyak <zoya.tretyak@just-eat.com>
2019-10-18 16:29:12 +0300

    string +1st additional

--- Exercises/strings/strings.js
+++ Exercises/strings/strings.js
@@ -26,3 +26,41 @@ const firstLetter1 = firstName[0].toUpperCase();
 const firstLetter2 = lastName[0].toUpperCase();
 
 console.log(firstLetter1 + firstName.slice(1) +' '+ firstLetter2 + lastName.slice(1));
+
+// # Additional
+
+// 1. Declare a function which will receive password string. Check is password strong enough:
+//   - should contain at least one capital letter;
+//   - length no less than 8 characters.
+//   - should contain at leas one special character.
+
+function passvordValidation(pwd){
+  
+  const regexp = /^[a-zA-Z0-9]*$/;
+  if (pwd.toLowerCase() === pwd) {
+    console.log('Pasword should include at least 1 UpperCase letter');
+    return false;
+  }
+      
+  if (pwd.toUpperCase() === pwd) {
+    console.log('Pasword should include at least 1 LowCase letter');
+    return false;
+  }
+      
+  if (regexp.test(pwd)) {
+    console.log('Pasword should be include at least 1 special character');
+    return false;
+  }
+      
+  if (pwd.length < 8) {
+    console.log('Pasword should be 8 or more characters');
+    return false;
+  }
+      
+  return true;
+}
+  
+console.log(passvordValidation('Pa$$Word'));
+
+
+// 2. Make function which will generate password suggestion based on passed password, and required strongest.
