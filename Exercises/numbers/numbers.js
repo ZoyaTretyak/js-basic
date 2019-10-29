2099d18e42462af2b94b0a4776a17caea893ea32
zoya tretyak <zoya.tretyak@just-eat.com>
2019-10-29 11:33:21 +0200

    numbers lint fix

--- Exercises/numbers/numbers.js
+++ Exercises/numbers/numbers.js
@@ -8,11 +8,11 @@ console.log(binaryNumber, octalNumber, hexadecimalNumber);
 // 2. Implement function `extractNumbers` which will leave only numbers in passed string.
 function extractNumbers (text) {
   let number = parseInt(text);
-if (isNaN(number) == true) {
-  return console.log('Text should starts with numbers');
-} else {
-  return console.log(number);
-}
+  if (isNaN(number) == true) {
+    return console.log('Text should starts with numbers');
+  } else {
+    return console.log(number);
+  }
 }
 // 3. Declare a variable with decimal value and convert to binary, octal and hexadecimal.
 let decimalNumber = 55;
@@ -24,10 +24,8 @@ const oct = decimalNumber.toString(8);
 console.log(hex, bin, oct);
 
 // 4. use `Math.random` to generate random value from `0` to `100`.
-
 function randomGenerator(min, max) {
   return Math.round(min + Math.random() * (max - min));
 }
 
 console.log(randomGenerator(0, 100));
-
