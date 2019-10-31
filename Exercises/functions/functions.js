02600b92065dfe9c0a6e9d3e7e51abb3a598f88a
zoya tretyak <zoya.tretyak@just-eat.com>
2019-10-31 18:06:25 +0200

    functions

--- Exercises/functions/functions.js
+++ Exercises/functions/functions.js
@@ -2,20 +2,21 @@
 // function declaration
 function functionDeclaration() {}
 // function expression
-const funcExpression = function() {}
+const funcExpression = function () {};
 // lambda function
 const funcArrow = (param) => {
   return 1;
-}
+};
 const funcArrow1 = (param) => 1;
 // function object
+/*eslint-disable */
 const funcObject = new Function('a', 'b', 'return a * b');
 console.log(funcObject(4,6));
 
 // 2. Get parameters amount of function `takeMyParametersAmount`.
 
 function takeMyParametersAmount(a, b, c) {
-  return a+b+c;
+  return a + b + c;
 }
 
 // 2. Create a function and log all passed parameters to it. Check the `length` of this function.
@@ -25,7 +26,7 @@ console.log(myNewFunc.length);
 
 // 3. Using anonymous function encapsulate function body from global scope.
 const myVar = 'piu';
-const anonymousFunc = function() {
-    console.log(myVar);
-}
+const anonymousFunc = function () {
+  console.log(myVar);
+};
 anonymousFunc();
