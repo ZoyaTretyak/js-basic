597dd408130bb3273d211b7b109d4d677311c486
zoya tretyak <zoya.tretyak@just-eat.com>
2019-10-31 18:05:53 +0200

    functions

--- Exercises/functions/functions.js
+++ Exercises/functions/functions.js
@@ -1,3 +1,31 @@
-function takeMyParametersAmount(a, b, c) {
-  return null;
+// 1. Create a function declaration, function expression, lambda function, and using function object.
+// function declaration
+function functionDeclaration() {}
+// function expression
+const funcExpression = function() {}
+// lambda function
+const funcArrow = (param) => {
+  return 1;
 }
+const funcArrow1 = (param) => 1;
+// function object
+const funcObject = new Function('a', 'b', 'return a * b');
+console.log(funcObject(4,6));
+
+// 2. Get parameters amount of function `takeMyParametersAmount`.
+
+function takeMyParametersAmount(a, b, c) {
+  return a+b+c;
+}
+
+// 2. Create a function and log all passed parameters to it. Check the `length` of this function.
+
+function myNewFunc(a, b, c, d, r) {}
+console.log(myNewFunc.length);
+
+// 3. Using anonymous function encapsulate function body from global scope.
+const myVar = 'piu';
+const anonymousFunc = function() {
+    console.log(myVar);
+}
+anonymousFunc();
